bc68d71e69b38898bbca20f044fa2b0939e09159
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2022-11-30 17:26:14 -0500

    Reorganize project buttons, style project buttons.

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from "react";
-import AnalyzeSourceProj from "../projects/AnalyzeSourceProj";
+import AutoTTRecorderProj from "../projects/AutoTTRecorderProj";
 import "../Projects.css";
 
 class ProjectsPage extends React.Component {
   constructor(props) {
     super(props);
     let state = {
-      curProject: <AnalyzeSourceProj/>
+      curProject: <AutoTTRecorderProj/>
     };
     this.state = state;
     this.setProjectView = this.setProjectView.bind(this);
@@ -24,11 +24,11 @@ class ProjectsPage extends React.Component {
       <div>
         <h1 className="project-page-header">Projects</h1>
         <div id="project-buttons">
-          <button className="project-button" onClick={this.setProjectView} value="AnalyzeSourceProj">analyze-source</button>
-          <button className="project-button" onClick={this.setProjectView} value="GnuAssemblerProj">agbasm (GNU Assembler Modifications)</button>
           <button className="project-button" onClick={this.setProjectView} value="AutoTTRecorderProj">Auto-TT-Recorder</button>
           <button className="project-button" onClick={this.setProjectView} value="AutoTTRecorderYTProj">CTGP Legacy Records</button>
           <button className="project-button" onClick={this.setProjectView} value="AutoTTRecorderGUIProj">Auto-TT-Recorder GUI</button>
+          <button className="project-button" onClick={this.setProjectView} value="AnalyzeSourceProj">analyze-source</button>
+          <button className="project-button" onClick={this.setProjectView} value="GnuAssemblerProj">agbasm (GNU Assembler Modifications)</button>
         </div>
         <div id="project-view">
           <Suspense fallback={<div>Loading...</div>}>{this.state.curProject}</Suspense>
